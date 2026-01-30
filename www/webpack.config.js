0f52f1309334ba222897d7c5babe29335c2a5697
AaronCasildo <aacasildor@gmail.com>
2026-01-29 19:12:11 -0800

    feat: initialize the development of the project environment and configurations

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,6 +1,11 @@
-const path = require('path');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-module.exports = {
+import path from 'path';
+import { fileURLToPath } from 'url';
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, "public"),
